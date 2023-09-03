9141874a1b7a0b7208b01db0a3483311405fd4fe
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2023-09-03 20:38:58 +1000

    Updates About text and removes contact section

--- src/scripts/aside.js
+++ src/scripts/aside.js
@@ -1,7 +1,7 @@
 const header = document.getElementById('header');
 const about = document.getElementById('about');
-const contact = document.getElementById('contact');
+// const contact = document.getElementById('contact');
 
 document.getElementById('asideHeader').innerHTML = header.innerHTML;
 document.getElementById('asideAbout').innerHTML = about.innerHTML;
-document.getElementById('asideContact').innerHTML = contact.innerHTML;
+// document.getElementById('asideContact').innerHTML = contact.innerHTML;
